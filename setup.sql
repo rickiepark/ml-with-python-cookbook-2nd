bbce83ae44b6e0a296550825f2cd430f317b0215
Haesun Park <haesunrpark@gmail.com>
2024-02-26 17:43:07 +0900

    setup.sql  수정

--- setup.sql
+++ setup.sql
@@ -8,9 +8,12 @@ CREATE TABLE `data` (
   `category` int(11) NOT NULL
 );
 
-LOAD DATA INFILE './data.csv' 
+LOAD DATA INFILE '/content/data.csv' 
 INTO TABLE data 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'root';
+FLUSH PRIVILEGES;
